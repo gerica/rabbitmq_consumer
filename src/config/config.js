0103e79aa09d63bcd811f2a9dc8ded75bb0ac516
gerica <rogeriogerica@gmail.com>
2021-06-01 19:53:38 -0300

    conf production

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,6 @@
 import dotenv from 'dotenv';
 
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config({ path: `${process.cwd()}/src/config/.env` });
-}
+dotenv.config({ path: `${process.cwd()}/src/config/.env` });
 
 export default {
   ...process.env,
