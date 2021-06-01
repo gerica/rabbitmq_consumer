ab2b914c1da297952483ebc1db77e361641c9795
gerica <rogeriogerica@gmail.com>
2021-06-01 18:54:01 -0300

    add version

--- src/graphql/resolvers/messageResolver.js
+++ src/graphql/resolvers/messageResolver.js
@@ -1,6 +1,7 @@
 import { MessageService } from '../../api/services/index.js';
 
 const query = {
+  versionRabbitMQConsumer: () => 'Version Radiolife RabbitMQ Consumer: 0.0.1-rc-01',
   getMessage: () => MessageService.getMessage(),
   getAllMessages: () => MessageService.getAllMessages(),
 };
